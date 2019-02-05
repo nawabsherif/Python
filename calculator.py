373593a56b37600ff4b02e3cea6b41edca628c58
K NAWAB SHERIF <47314338+nawabsherif@users.noreply.github.com>
2019-02-05 10:38:23 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,7 @@
 print("1.add")
 print("2.mul")
+print("3.div")
+print("4.mod")
 choice=int(raw_input("Enter your choice "))
 num1=int(input("num1 is:"))
 num2=int(input("num2 is:"))
@@ -7,5 +9,9 @@ if(choice==1):
   print(num1+num2)
 elif(choice==2):
   print(num1*num2)
+elif(choice==3):
+  print(num1/num2)
+elif(choice==4):
+  print(num1%num2)
 else:
   print(num1-num2)
