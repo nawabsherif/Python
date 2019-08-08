503873a477e1157b829948a87a6123fa3dfc63f2
K NAWAB SHERIF <47314338+nawabsherif@users.noreply.github.com>
2019-08-08 10:35:24 +0530

    Update leapyear.py

--- leapyear.py
+++ leapyear.py
@@ -1,9 +1,6 @@
-a=int(input())
-if(a%400==0):
-    print ("leapyear")
-elif(a%4==0):
-    print ("leapyear")
-elif(a%100!=0):
-    print ("leapyear")
+year = int(input("Please Enter the Year Number you wish: "))
+
+if (( year%400 == 0)or (( year%4 == 0 ) and ( year%100 != 0))):
+    print("%d is a Leap Year" %year)
 else:
-    print ("not leapyear")
+    print("%d is Not the Leap Year" %year)
